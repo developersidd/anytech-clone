2214173f75930045f9f4ac21b4fed085b5f39b2b
Emon8794 <emonabujafor@gmail.com>
2025-02-03 02:58:14 +0600

    Add alt-text eslint disable comment in Icon component

--- src/components/common/Icon.tsx
+++ src/components/common/Icon.tsx
@@ -1,6 +1,7 @@
 import Image, { ImageProps } from "next/image";
 
 function Icon(props: ImageProps) {
+  // eslint-disable-next-line jsx-a11y/alt-text
   return <Image {...props} width={64} height={64} />;
 }
 
